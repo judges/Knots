8716057262f775814fdeea831b4d46ae20384267
Andrew Daskaliuk <drongous@gmail.com>
2010-10-20 13:34:52 +0300

    Add phoneG

---
+++ PhoneGa/Classes/PhoneGaAppDelegate.h
@@ -0,0 +1,16 @@
+//
+//  PhoneGaAppDelegate.h
+//  PhoneGa
+//
+//  Created by Andrew Daskalyuk on 10/19/10.
+//  Copyright __MyCompanyName__ 2010. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "PhoneGapDelegate.h"
+
+@interface PhoneGaAppDelegate : PhoneGapDelegate {
+}
+
+@end
+
